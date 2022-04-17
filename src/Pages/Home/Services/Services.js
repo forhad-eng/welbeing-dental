39a27cf84abb93402f2be74404f91d5410f56028
forhad-eng <forhadu.cse@gmail.com>
2022-04-17 20:02:46 +0600

    facebook login system integrated

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import useServices from '../../../hooks/useServices'
+import LoadingSpinner from '../../Shared/LoadingSpinner/LoadingSpinner'
 import Service from '../Service/Service'
 
 const Services = () => {
     const [services] = useServices()
 
     return (
-        <div className="px-4 md:px-16">
-            <p className="text-3xl text-center text-gray-700 font-[500] my-10 pb-2 tracking-wider">
-                Our Services
-                <hr style={{ height: '2px' }} className="block mx-auto w-24 bg-gray-400" />
-            </p>
+        <>
+            {services.length === 0 ? (
+                <LoadingSpinner />
+            ) : (
+                <div className="px-4 md:px-16">
+                    <p className="text-3xl text-center text-gray-700 font-[500] my-10 pb-2 tracking-wider">
+                        Our Services
+                        <hr style={{ height: '2px' }} className="block mx-auto w-24 bg-gray-400" />
+                    </p>
 
-            <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-10 my-10">
-                {services.slice(0, 4).map(service => (
-                    <Service key={service.id} service={service} />
-                ))}
-            </div>
-        </div>
+                    <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-10 my-10">
+                        {services.slice(0, 4).map(service => (
+                            <Service key={service.id} service={service} />
+                        ))}
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
 
--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,16 +1,23 @@
 import React from 'react'
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import { useSignInWithFacebook, useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import toast from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
 import { auth } from '../../../Firebase/firebase.init'
 import facebook from '../../../images/icons/fb.png'
 import google from '../../../images/icons/google.png'
 
 const SocialLogin = () => {
-    const [signInWithGoogle, user] = useSignInWithGoogle(auth)
+    const [signInWithGoogle, googleUser, , gError] = useSignInWithGoogle(auth)
+    const [signInWithFacebook, facebookUser, , fError] = useSignInWithFacebook(auth)
     const navigate = useNavigate()
 
-    if (user) {
+    if (googleUser || facebookUser) {
+        toast.success('Success', { id: 'loginSuccess' })
         navigate('/')
+    }
+
+    if (gError || fError) {
+        toast.error(`${gError?.message} ${fError?.message}`, { id: 'loginError' })
     }
 
     return (
@@ -23,7 +30,10 @@ const SocialLogin = () => {
                 <p className="mx-5 font-semibold">Continue with Google</p>
             </button>
 
-            <button className="w-full flex items-center bg-white p-1 border-2 border-gray-300 rounded-full outline-none">
+            <button
+                onClick={() => signInWithFacebook()}
+                className="w-full flex items-center bg-white p-1 border-2 border-gray-300 rounded-full outline-none"
+            >
                 <img width={'37'} src={facebook} alt="Facebook" />
                 <p className="mx-5 font-semibold">Continue with Facebook</p>
             </button>
