a62c979d5c4fe3f173d7f6637116b53f3221e943
forhad-eng <forhadu.cse@gmail.com>
2022-04-18 14:13:37 +0600

    home section responsived

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -10,11 +10,12 @@ import Services from './Services/Services'
 const Home = () => {
     return (
         <section>
-            <div className="home-banner grid md:grid-cols-2 md:gap-10 md:items-center px-4 md:px-16">
-                <div className="order-2 md:order-1">
+            <div className="home-banner grid md:grid-cols-2 lg:gap-10 lg:px-10">
+                <div className="order-2 md:order-1 md:flex items-center">
                     <img width={'530'} src={dentalPic} alt="" />
                 </div>
-                <div className="ml-52 mt-6 md:mb-20 md:ml-40 order-1 md:order-2">
+
+                <div className="mt-6 md:mt-24 ml-44 md:ml-20 lg:ml-40 pr-2 order-1 md:order-2">
                     <p className="text-2xl uppercase mb-2">Welcome to</p>
                     <p className="text-2xl md:text-5xl mb-2 md:mb-7">
                         <span className="font-bold tracking-wide">Wellbeing</span> Dental
